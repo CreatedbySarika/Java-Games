a9513fcf420a7524229066936ee75097f48705fd
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-25 23:37:09 +0100

    frame

--- games/TTT.java
+++ games/TTT.java
@@ -1,6 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+
 public class TTT {
     public static void main(String[] args) {
         System.out.println("Tic-Tac-toe");
+
+        JFrame fr = new JFrame("Tic Tac Toe");
+        fr.setLocation(800, 255);
+        fr.setSize(450, 250);
+        fr.setResizable(true);
+        fr.setVisible(true);
 
     }
 }

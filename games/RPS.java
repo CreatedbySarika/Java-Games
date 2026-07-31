94ef9cbe2077567187669b69e523b017cc740433
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-31 23:37:01 +0100

    rps frame

--- games/RPS.java
+++ games/RPS.java
@@ -1,6 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class RPS {
     public static void main(String[] args) {
         System.out.println("Rock paper scissor");
+
+        JFrame fr = new JFrame("Rps");
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setVisible(true);
 
     }
 }

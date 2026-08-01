ecd4df67e0b9277ae9ca50e1a01857dc4bcc9f12
CreatedBySarika <sahsarika2005@gmail.com>
2026-08-01 23:50:30 +0100

    math class

--- games/RPS.java
+++ games/RPS.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import Math;
 
 public class RPS {
     public static void main(String[] args) {
